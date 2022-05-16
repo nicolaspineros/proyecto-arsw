c034a4acd632b6fe49bad7f29078bc443a3adbfe
Julian Peña <julian.pena-m@mail.escuelaing.edu.co>
2022-05-15 21:08:04 -0500

    borrando clases que no se usan

--- src/main/java/edu/eci/arsw/warGameApi/prueba.java
+++
@@ -1,9 +0,0 @@
-package edu.eci.arsw.warGameApi;
-
-import edu.eci.arsw.warGame.persistence.ConnectionMongo;
-
-public class prueba {
-    public static void main(String[] args) {
-        ConnectionMongo conexion = new ConnectionMongo();
-    }
-}
